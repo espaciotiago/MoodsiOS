a9448fc73d20ea816c918c261bf99ce9c3a21043
Santiago Moreno <smoreno@opalo.com.co>
2018-01-03 12:17:15 -0500

    Initial Commit

---
+++ Moods/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Moods
+//
+//  Created by Santiago Moreno on 3/01/18.
+//  Copyright © 2018 Tiago Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
